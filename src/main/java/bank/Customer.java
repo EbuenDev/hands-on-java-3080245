45eedcbdba8c1985fcdd5066590806353d36bc70
Mark Ian Ebuen <ianebuen2001@gmail.com>
2025-07-21 13:54:28 +0800

    Create a Customers Class
    This class is a constructor class with a getter and setter

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,49 @@
+package bank;
+
+public class Customer {
+  private int id;
+  private String username;
+  private String password;
+  private int accountId;
+
+
+  public Customer(int id, String username, String password, int accountId) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.accountId = accountId;
+  }
+
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getAccountId() {
+    return accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+}
