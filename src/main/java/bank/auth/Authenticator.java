d22a2e374d7121df71cbfad20bc2ed234e135038
Mark Ian Ebuen <ianebuen2001@gmail.com>
2025-07-22 02:40:34 +0800

    Add a new class name authenticator under the auth package
    this allows us to authenticate the user using the java security auth, in addition i add new constructor name authenticated in the customers account since the credential is in that constructor.

---
+++ src/main/java/bank/auth/Authenticator.java
@@ -0,0 +1,30 @@
+package bank.auth;
+
+import javax.security.auth.login.LoginException;
+
+import bank.constructor.Customer;
+
+public class Authenticator {
+  
+  public static void login(String username, String password) throws LoginException {
+    Customer customer = bank.model.CustomerModel.getCustomer(username);
+
+    if (customer == null) {
+        throw new LoginException("Customer not found");
+    }
+
+    if (password.equals(customer.getPassword())) {
+        customer.setAuthenticated(true);  // Set the customer as authenticated.
+        System.out.println("Login successful for user: " + customer.getUsername());
+    }
+
+    else throw new LoginException("Invalid password for user: " + customer.getUsername());
+
+  }
+
+
+  public static void logout(Customer customer) {
+    customer.setAuthenticated(false);  // Set the customer as not authenticated.
+    System.out.println("Logout successful for user: " + customer.getUsername());
+  }
+}
--- src/main/java/bank/constructor/Customer.java
+++ src/main/java/bank/constructor/Customer.java
@@ -1,21 +1,23 @@
 package bank.constructor;
 
 public class Customer {
-  private int id;
-  private String name;
-  private String username;
-  private String password;
-  private int accountId;
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private int accountId;
+    private Boolean authenticated;
 
-  public Customer(int id,String name, String username, String password, int accountId) {
+    public Customer(int id,String name, String username, String password, int accountId) {
     
     this.id = id;
     this.name = name;
     this.username = username;
     this.password = password;
     this.accountId = accountId;
+    this.authenticated = false;  // Initially, the customer is not logged in.
     
-  }
+}
 
     public int getId() {
         return id;
@@ -57,5 +59,13 @@ public class Customer {
         this.name = name;
     }
 
-  
+    public Boolean isAuthenticated() {
+        return authenticated;
+    }
+    
+    public void setAuthenticated(Boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+
 }
